ec214fe370d266ea75244c627cf7fdfabaf7f761
Merseyside <ivanklessablin@gmail.com>
2021-12-12 16:18:27 +0700

    ++ver 1.0.5 Add logger ext

---
+++ kotlin-ext/src/commonMain/kotlin/com/merseyside/merseyLib/kotlin/extensions/LoggerExt.kt
@@ -0,0 +1,18 @@
+package com.merseyside.merseyLib.kotlin.extensions
+
+import com.merseyside.merseyLib.kotlin.Logger
+import com.merseyside.merseyLib.kotlin.Logger.log as loggerLog
+
+fun <T> T.log(tag: Any = Logger.TAG, prefix: Any? = null, suffix: Any? = null): T {
+    val msg = "${prefix ?: ""} ${Logger.adoptMsg(this)} ${suffix ?: ""}"
+    Logger.log(tag, msg)
+
+    return this
+}
+
+inline fun <reified T> T.logMsg(
+    tag: String = T::class.simpleName ?: Logger.TAG,
+    message: Any? = null
+): T {
+    return this.also { loggerLog(tag, message) }
+}
---
+++ publishToMavenLocal.sh
@@ -0,0 +1,1 @@
+./gradlew -PlibraryPublish :kotlin-ext:publishToMavenLocal
